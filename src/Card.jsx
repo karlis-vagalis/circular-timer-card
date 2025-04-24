fac3e3b5b3d44c2fa61913ad624ddb715f34917b
VAGALIS, Karlis <karlis.vagalis@technica-engineering.de>
2025-04-24 18:07:24 +0200

    Added exposure of setConfig

--- src/Card.jsx
+++ src/Card.jsx
@@ -2,15 +2,19 @@ import { onMount, createSignal } from "solid-js";
 import "./Card.css";
 
 export const Card = (props) => {
+	const [config, setConfig] = createSignal(props.config || {});
+	const [timer, setTimer] = createSignal();
 
-    const [timer, setTimer] = createSignal();
+	Card.prototype.setConfig = (newConfig) => {
+		setConfig(newConfig);
+	};
 
-    onMount(() => {
-        console.log(props)
-        if (window.hass) {
-            console.log(window.hass)
-            setTimer(window.hass.states[props.id]);
-        }
-    })
+	onMount(() => {
+		console.log(props);
+		if (window.hass) {
+			console.log(window.hass);
+			setTimer(window.hass.states[props.id]);
+		}
+	});
 	return <div>Examples</div>;
 };
