f5d88958b335c06cb36e43c3dba33fd555c8fc64
VAGALIS, Karlis <karlis.vagalis@technica-engineering.de>
2025-04-24 16:36:06 +0200

    Build stage

--- rspack.config.js
+++ rspack.config.js
@@ -1,23 +1,26 @@
-import { defineConfig } from '@rspack/cli';
+import { defineConfig } from "@rspack/cli";
 
 export default defineConfig({
-  entry: {
-    main: './src/index.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.jsx$/,
-        use: [
-          {
-            loader: 'babel-loader',
-            options: {
-              presets: ['solid'],
-              plugins: ['solid-refresh/babel'],
-            },
-          },
-        ],
-      },
-    ],
-  },
+	entry: {
+		main: "./src/index.js",
+	},
+	resolve: {
+		extensions: [".js", ".jsx"],
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx$/,
+				use: [
+					{
+						loader: "babel-loader",
+						options: {
+							presets: ["solid"],
+							plugins: ["solid-refresh/babel"],
+						},
+					},
+				],
+			},
+		],
+	},
 });
---
+++ src/index.js
@@ -0,0 +1,4 @@
+import { customElement } from "solid-element";
+import { Card } from "./Card.jsx";
+
+customElement("circular-timer-card", Card);
