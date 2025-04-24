df52bc6908b3acac50223b7a9b9bcf1e6f4b8923
VAGALIS, Karlis <karlis.vagalis@technica-engineering.de>
2025-04-24 17:56:37 +0200

    Updated versioning mechanism

--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
+import pkg from '../package.json';
 import { customElement } from "solid-element";
 import { Card } from "./Card.jsx";
 
 customElement("circular-timer-card", {}, Card);
 
 console.info(
-	`%c circular-timer-card | Version 1.1 `,
+	`%c circular-timer-card | Version ${pkg.version} `,
 	"color: white; font-weight: bold; background: #FF4F00",
 );
