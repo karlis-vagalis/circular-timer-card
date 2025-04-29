279a0295a7d867cc8962d4d2898c8976e6f2e6df
VAGALIS, Karlis <karlis.vagalis@gmail.com>
2025-04-29 17:11:57 +0200

    Added formatting utils

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,63 +1,61 @@
 import {
-	createSignal,
-	createEffect,
-	mergeProps,
-	Show,
-	Match,
-	Switch,
+  createSignal,
+  createEffect,
+  mergeProps,
+  Show,
+  Match,
+  Switch,
 } from "solid-js";
 import style from "./Card.css";
 import { Warning } from "./components/Warning.jsx";
-import { entityExistsAndIsValid, getRemaining } from "./lib.js";
+import { entityExistsAndIsValid, getRemaining, formatDuration } from "./lib.js";
 import { createStore } from "solid-js/store";
 
 export const Card = (props) => {
-	props = mergeProps(
-		{
-			config: {
-				style: "circle",
-				progress: {
-					count: 1,
-				},
-			},
-		},
-		props,
-	);
-	const [remaining, setRemaining] = createStore({
-		progress: 0,
-		duration: {
-			seconds: 0,
-			minutes: 0,
-			hours: 0,
-		},
-	});
-	const [entity, setEntity] = createSignal();
+  props = mergeProps(
+    {
+      config: {
+        style: "circle",
+        progress: {
+          count: 1,
+        },
+      },
+    },
+    props,
+  );
+  const [remaining, setRemaining] = createStore({
+    progress: 0,
+    duration: {
+      seconds: 0,
+      minutes: 0,
+      hours: 0,
+    },
+  });
+  const [entity, setEntity] = createSignal();
 
-	createEffect(() => {
-		const entityId = props.config?.entity;
-		setEntity(
-			entityExistsAndIsValid(props.config, props.hass) ? entityId : undefined,
-		);
-	});
+  createEffect(() => {
+    const entityId = props.config?.entity;
+    setEntity(
+      entityExistsAndIsValid(props.config, props.hass) ? entityId : undefined,
+    );
+  });
 
-	createEffect(() => {
-		setRemaining(getRemaining(entity(), props.hass));
-	});
+  createEffect(() => {
+    setRemaining(getRemaining(entity(), props.hass));
+  });
 
-	return (
-		<>
-			<style>{style}</style>
-			<ha-card classList={{ "bg-red-900": !entity() }}>
-				<Show when={!entity()}>
-					<Warning message="Card configuration does not contain 'entity' setting or the provided ID is invalid!" />
-				</Show>
-				<Switch>
-					<Match when={props.config.style === "circle"}></Match>
-				</Switch>
-				<Show when={entity()}>
-					{remaining.duration.hours}:{remaining.duration.minutes}:{remaining.duration.seconds}
-				</Show>
-			</ha-card>
-		</>
-	);
+  return (
+    <>
+      <style>{style}</style>
+      <ha-card classList={{ "bg-red-900": !entity() }}>
+        <Show when={!entity()}>
+          <Warning message="Card configuration does not contain 'entity' setting or the provided ID is invalid!" />
+        </Show>
+        <Switch>
+          <Match when={props.config.style === "circle"}></Match>
+        </Switch>
+        <Show when={entity()}>{formatDuration(remaining.duration)}</Show>
+      </ha-card>
+    </>
+  );
 };
--- src/lib.js
+++ src/lib.js
@@ -1,88 +1,96 @@
 export const entityExistsAndIsValid = (config, hass) => {
-	const id = config?.entity;
-	return (
-		Object.hasOwn(config, "entity") &&
-		typeof id === "string" &&
-		id in hass.states
-	);
+  const id = config?.entity;
+  return (
+    Object.hasOwn(config, "entity") &&
+    typeof id === "string" &&
+    id in hass.states
+  );
 };
 
 const getDomain = (entityId) => {
-	return entityId.split(".")[0];
+  return entityId.split(".")[0];
 };
 
 const parseDuration = (content) => {
-	const { hours, minutes, seconds } =
-		/^(?<hours>[0-9]{1,2}):(?<minutes>[0-9]{1,2}):(?<seconds>[0-9]{1,2})$/gm.exec(
-			content,
-		).groups;
-	return {
-		hours: parseInt(hours),
-		minutes: parseInt(minutes),
-		seconds: parseInt(seconds),
-	};
+  const { hours, minutes, seconds } =
+    /^(?<hours>[0-9]{1,2}):(?<minutes>[0-9]{1,2}):(?<seconds>[0-9]{1,2})$/gm.exec(
+      content,
+    ).groups;
+  return {
+    hours: parseInt(hours),
+    minutes: parseInt(minutes),
+    seconds: parseInt(seconds),
+  };
 };
 
 const toSeconds = (duration) => {
-	return duration.seconds + duration.minutes * 60 + duration.hours * 60 * 60;
+  return duration.seconds + duration.minutes * 60 + duration.hours * 60 * 60;
 };
 
 const toDuration = (seconds) => {
-	return parseDuration(
-		new Date(seconds * 1000).toISOString().substring(11, 16),
-	);
+  return parseDuration(
+    new Date(seconds * 1000).toISOString().substring(11, 16),
+  );
 };
 
 const secondsTillFinish = (finishesAt) => {
-	return (Date.parse(finishesAt) - new Date()) / 1000;
+  return (Date.parse(finishesAt) - new Date()) / 1000;
 };
 
 const getProgress = (remainingDuration, totalDuration) => {
-	let total = toSeconds(totalDuration);
-	if (total === 0) total++;
-	return toSeconds(remainingDuration) / total;
+  let total = toSeconds(totalDuration);
+  if (total === 0) total++;
+  return toSeconds(remainingDuration) / total;
+};
+
+const pad = (number) => {
+  return String(number).padStart(2, "0");
+};
+
+export const formatDuration = (duration) => {
+  return `${pad(duration.hours)}:${pad(duration.minutes)}:${pad(duration.seconds)}`;
 };
 
 export const getRemaining = (entityId, hass) => {
-	const state = hass.states[entityId];
-	const domain = getDomain(entityId);
+  const state = hass.states[entityId];
+  const domain = getDomain(entityId);
 
-	console.log(domain, state);
+  console.log(domain, state);
 
-	let remaining = {
-		hours: 0,
-		minutes: 0,
-		seconds: 0,
-	};
-	let total = {
-		hours: 0,
-		minutes: 0,
-		seconds: 0,
-	};
+  let remaining = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
+  let total = {
+    hours: 0,
+    minutes: 0,
+    seconds: 0,
+  };
 
-	switch (domain) {
-		case "timer":
-			total = parseDuration(state.attributes.duration);
-			switch (state.state) {
-				case "active":
-					remaining = toDuration(
-						secondsTillFinish(state.attributes.finishes_at),
-					);
-					break;
-				case "paused":
-					remaining = parseDuration(state.attributes.remaining);
-					break;
-				case "idle":
-					remaining = total;
-					break;
-			}
-			break;
-	}
+  switch (domain) {
+    case "timer":
+      total = parseDuration(state.attributes.duration);
+      switch (state.state) {
+        case "active":
+          remaining = toDuration(
+            secondsTillFinish(state.attributes.finishes_at),
+          );
+          break;
+        case "paused":
+          remaining = parseDuration(state.attributes.remaining);
+          break;
+        case "idle":
+          remaining = total;
+          break;
+      }
+      break;
+  }
 
-	const progress = getProgress(remaining, total);
+  const progress = getProgress(remaining, total);
 
-	return {
-		progress,
-		duration: remaining,
-	};
+  return {
+    progress,
+    duration: remaining,
+  };
 };
