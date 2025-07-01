43202b778dc5038f43de990b707093763e7e3632
VAGALIS, Karlis <karlis.vagalis@gmail.com>
2025-07-01 19:21:25 +0200

    Types

--- src/Card.tsx
+++ src/Card.tsx
@@ -12,7 +12,7 @@ import { DurationString } from "./components/DurationString.tsx";
 import { ProgressBar } from "./components/ProgressBar.tsx";
 import { Warning } from "./components/Warning.tsx";
 import { entityExistsAndIsValid, getRemaining } from "./lib.ts";
-import type { Config } from "./types.ts";
+import type { Config, Duration } from "./types.ts";
 
 const defaultConfig: Config = {
 	layout: "circle",
@@ -45,7 +45,10 @@ export const Card = (props: { config: Config; hass: any }) => {
 		},
 		props,
 	);
-	const [remaining, setRemaining] = createStore({
+	const [remaining, setRemaining] = createStore<{
+		progress: number;
+		duration: Duration;
+	}>({
 		progress: 0,
 		duration: {
 			seconds: 0,
